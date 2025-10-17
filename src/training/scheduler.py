de46caa5acc838b9b268ca53e967bf756e2724e6
Priyangkar Ghosh <22mp50@queensu.ca>
2025-10-17 01:38:57 -0400

    update cosine decay scheduler so it inherits torch.LRScheduler

--- src/training/scheduler.py
+++ src/training/scheduler.py
@@ -1,14 +1,16 @@
 import math
 from torch.optim import Optimizer
+from torch.optim.lr_scheduler import LRScheduler
 
-class CosineDecayLR:
+class CosineDecayLR(LRScheduler):
     def __init__(
         self, 
         optimizer: Optimizer, 
         max_lr: float = 1e-3, 
         min_lr: float = 1e-4, 
         warmup_steps: int = 100, 
-        total_steps: int = 1000
+        total_steps: int = 1000,
+        previous_step: int = -1,
     ) -> None:
         self.optimizer = optimizer
         
@@ -18,26 +20,29 @@ class CosineDecayLR:
         self.warmup_steps = warmup_steps
         self.total_steps = total_steps
         
-        self.current_step: int = 0
-
-    def step(self):
-        self.current_step += 1
-        lr = self.get_lr(self.current_step)
-        for param_group in self.optimizer.param_groups:
-            param_group['lr'] = lr
-        return lr
-
-    def get_lr(self, it):
+        # set initial learning rates for all param groups
+        for group in optimizer.param_groups:
+            group.setdefault('initial_lr', max_lr)
+        
+        # init parent
+        super().__init__(optimizer, previous_step)
+        
+    @property
+    def current_step(self): # pytorch weird naming so use this for clarity
+        return self.last_epoch
+    
+    def get_lr(self):
         # linear warmup
-        if it < self.warmup_steps:
-            return self.max_lr * (it + 1) / self.warmup_steps
+        if self.last_epoch < self.warmup_steps:
+            return self.max_lr * (self.last_epoch + 1) / self.warmup_steps
 
         # reached total steps
-        if it > self.total_steps: return self.min_lr
+        if self.last_epoch > self.total_steps: return self.min_lr
         
         # cosine decay
-        decay = (it - self.warmup_steps) / max(1, self.total_steps - self.warmup_steps)
+        decay = (self.last_epoch - self.warmup_steps) / max(1, self.total_steps - self.warmup_steps)
         assert 0 <= decay <= 1
         coeff = 0.5 * (1 + math.cos(math.pi * decay))
-        return self.min_lr + coeff * (self.max_lr - self.min_lr)
+        lr = self.min_lr + coeff * (self.max_lr - self.min_lr)
+        return [lr for _ in self.optimizer.param_groups]
     
